ee91c1d10bf5a4a48bc90c8e75c62c360790530e
jfarn <jfarnault29@gmail.com>
2024-04-27 12:15:57 +0200

    Programme fonctionnel d'affichage

--- Projet/Core/Src/freertos.c
+++ Projet/Core/Src/freertos.c
@@ -39,14 +39,15 @@
 /* USER CODE BEGIN PTD */
 typedef struct
 {
- 	int16_t dx;
-  int16_t dy;
+	int32_t dx;
+	int32_t dy;
 }
 mouvement;
 /* USER CODE END PTD */
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
+
 
 /* USER CODE END PD */
 
@@ -63,22 +64,24 @@ uint8_t workBuffer[2 * _MAX_SS];
 char *pDirectoryFiles[MAX_BMP_FILES];
 uint8_t ubNumberOfFiles = 0;
 uint32_t uwBmplen = 0;
+char image[32778];	//32778	130700
+BYTE data[960]; //480*2
 
 uint8_t *uwInternelBuffer; //Buffer pour la mémoire SDRAM
 /* USER CODE END Variables */
 osThreadId defaultTaskHandle;
-osThreadId task_Affich_PicHandle;
 osThreadId tackdeplacementHandle;
 osThreadId DisplayHandle;
 osMessageQId DeplacementQueueHandle;
 
 /* Private function prototypes -----------------------------------------------*/
 /* USER CODE BEGIN FunctionPrototypes */
+void RemplirImage(int32_t x, int32_t y, char* image);
+void FabriquerEntete(char* image);
 
 /* USER CODE END FunctionPrototypes */
 
 void StartDefaultTask(void const * argument);
-void Affichage_Pic(void const * argument);
 void deplacement_fonction(void const * argument);
 void Display_fonction(void const * argument);
 
@@ -178,19 +181,15 @@ void MX_FREERTOS_Init(void) {
 
   /* Create the thread(s) */
   /* definition and creation of defaultTask */
-  osThreadDef(defaultTask, StartDefaultTask, osPriorityNormal, 0, 4096);
+  osThreadDef(defaultTask, StartDefaultTask, osPriorityNormal, 0, 128);
   defaultTaskHandle = osThreadCreate(osThread(defaultTask), NULL);
 
-  /* definition and creation of task_Affich_Pic */
-  osThreadDef(task_Affich_Pic, Affichage_Pic, osPriorityNormal, 0, 2048);
-  task_Affich_PicHandle = osThreadCreate(osThread(task_Affich_Pic), NULL);
-
   /* definition and creation of tackdeplacement */
-  osThreadDef(tackdeplacement, deplacement_fonction, osPriorityRealtime, 0, 128);
+  osThreadDef(tackdeplacement, deplacement_fonction, osPriorityRealtime, 0, 1024);
   tackdeplacementHandle = osThreadCreate(osThread(tackdeplacement), NULL);
 
   /* definition and creation of Display */
-  osThreadDef(Display, Display_fonction, osPriorityIdle, 0, 1024);
+  osThreadDef(Display, Display_fonction, osPriorityIdle, 0, 4096);
   DisplayHandle = osThreadCreate(osThread(Display), NULL);
 
   /* USER CODE BEGIN RTOS_THREADS */
@@ -217,39 +216,6 @@ void StartDefaultTask(void const * argument)
   /* USER CODE END StartDefaultTask */
 }
 
-/* USER CODE BEGIN Header_Affichage_Pic */
-/**
-* @brief Function implementing the task_Affich_Pic thread.
-* @param argument: Not used
-* @retval None
-*/
-/* USER CODE END Header_Affichage_Pic */
-void Affichage_Pic(void const * argument)
-{
-  /* USER CODE BEGIN Affichage_Pic */
-		FATFS fs0;
-		FIL file;
-		BYTE buffer[99];
-		UINT br;
-
-//		uint8_t str[30];
-		uwInternelBuffer = (uint8_t*) 0xC0260000;
-//		uint16_t MessageFromIHM;
-
-		/*##-1- Link the SD Card disk I/O driver ###################################*/
-		if (FATFS_LinkDriver(&SD_Driver, SDPath) == 0) {
-			/*##-2- Initialize the Directory Files pointers (heap) ###################*/
-			f_mount(&fs0, "", 0);
-			f_open(&file,"mapENS.bmp",FA_READ);
-			f_read(&file,(TCHAR *)buffer,sizeof buffer,&br);
-			f_close(&file);
-		} else {
-			/* FatFs Initialization Error */
-			Error_Handler();
-		}
-  /* USER CODE END Affichage_Pic */
-}
-
 /* USER CODE BEGIN Header_deplacement_fonction */
 /**
 * @brief Function implementing the tackdeplacement thread.
@@ -259,43 +225,51 @@ void Affichage_Pic(void const * argument)
 /* USER CODE END Header_deplacement_fonction */
 void deplacement_fonction(void const * argument)
 {
-  /* USER CODE BEGIN deplacement_fonction */
-	  TS_StateTypeDef TS_State;
-	  int16_t touch,x,y,x0,y0;
-	  mouvement deplacement;
-	  char text[50]={};
-	  /* Infinite loop */
-	  for(;;)
-	  {
-	    BSP_TS_GetState(&TS_State);
-	    if(TS_State.touchDetected){
-	      sprintf(text,"x %d y %d            ",TS_State.touchX[0],TS_State.touchY[0]);
-	      BSP_LCD_DisplayStringAtLine(4, (uint8_t*) text);
-	      if (touch==0){
-			    x0=TS_State.touchX[0];
-	        y0=TS_State.touchY[0];
-	        touch=1;
-		  	}
-	      else{
-	        x=TS_State.touchX[0];
-	        y=TS_State.touchY[0];
-	    	}
-	    }
-	    else {
-	        touch=0;
-	        deplacement.dx=x0-x;
-	        deplacement.dy=y0-y;
-	        sprintf(text,"dx %d dy %d            ",deplacement.dx,deplacement.dy);
-	        BSP_LCD_DisplayStringAtLine(1, (uint8_t*) text);
-	        sprintf(text,"f: x %d y %d            ",x,y);
-	        BSP_LCD_DisplayStringAtLine(2, (uint8_t*) text);
-	        sprintf(text,"d: x %d y %d            ",x0,y0);
-	        BSP_LCD_DisplayStringAtLine(3, (uint8_t*) text);
-	        xQueueSend(DeplacementQueueHandle, &deplacement, 0);
-	    }
-	    osDelay(50);
-	  }
-  /* USER CODE END deplacement_fonction */
+	/* USER CODE BEGIN deplacement_fonction */
+	TS_StateTypeDef TS_State;
+	int32_t x=0,y=0,x0=0,y0=0;
+	uint8_t verrouillage=0,touch=0;
+	mouvement deplacement;
+	char text[50]={};
+	/* Infinite loop */
+	for(;;)
+	{
+		BSP_TS_GetState(&TS_State);
+		if(TS_State.touchDetected){
+			// sprintf(text,"x %d y %d            ",TS_State.touchX[0],TS_State.touchY[0]);
+			// BSP_LCD_DisplayStringAtLine(4, (uint8_t*) text);
+			if ((TS_State.touchX[0]<40)&& (TS_State.touchY[0]<40)){
+				verrouillage=!verrouillage;
+			}
+			if (verrouillage==1) BSP_LCD_FillCircle(TS_State.touchX[0], TS_State.touchY[0], 4);
+			else{
+				if (touch==0){
+					x0=TS_State.touchX[0];
+					y0=TS_State.touchY[0];
+					touch=1;
+				}
+				else{
+					x=TS_State.touchX[0];
+					y=TS_State.touchY[0];
+				}
+			}
+		}
+		else if(touch!=0) {
+			touch=0;
+			deplacement.dx=x0-x;
+			deplacement.dy=y-y0;
+			//			sprintf(text,"dx %d dy %d            ",deplacement.dx,deplacement.dy);
+			//			BSP_LCD_DisplayStringAtLine(1, (uint8_t*) text);
+			//			sprintf(text,"f: x %d y %d            ",x,y);
+			//			BSP_LCD_DisplayStringAtLine(2, (uint8_t*) text);
+			//			sprintf(text,"d: x %d y %d            ",x0,y0);
+			//			BSP_LCD_DisplayStringAtLine(3, (uint8_t*) text);
+			xQueueSend(DeplacementQueueHandle, &deplacement, 0);
+		}
+		if (verrouillage!=0)BSP_LCD_DisplayStringAtLine(1, (uint8_t*) "verrouillage");
+		osDelay(10);
+	}
+	/* USER CODE END deplacement_fonction */
 }
 
 /* USER CODE BEGIN Header_Display_fonction */
@@ -308,23 +282,33 @@ void deplacement_fonction(void const * argument)
 void Display_fonction(void const * argument)
 {
 	/* USER CODE BEGIN Display_fonction */
-	 mouvement deplacement;
-	 char image[32778];
-	 int16_t x=0,y=0;
-	 FabriquerEntete(image);
+	mouvement deplacement;
+	uint16_t hauteur=272;
+	//char image[32778];	//32778
+	int32_t x=0,y=0;
+	char text[50]={};
+	FabriquerEntete(image);
+	for (int i=0;i<=8;i++){
+		RemplirImage(x,y+34*i,image);
+		BSP_LCD_DrawBitmap(0, hauteur-34*i, (uint8_t*) image);
+	}
 	/* Infinite loop */
 	for (;;) {
-	 if (xQueueReceive(DeplacementQueueHandle, &deplacement, 0)) {
-	   HAL_GPIO_TogglePin(LED13_GPIO_Port, LED13_Pin);
-	   x+=deplacement.dx;
-	   y+=deplacement.dy;
-	   if (x<0) x=0;
-	   if (x>227) x=1227;//1707-480
-	   if (y<0) y=0;
-	   if (y>428) y=428;//700-272
-	   RemplirImage(x,y,image);
-	   BSP_LCD_DrawBitmap(0, 0, (uint8_t*) image);
-	 }
+		if (xQueueReceive(DeplacementQueueHandle, &deplacement, 0)) {
+			HAL_GPIO_TogglePin(LED13_GPIO_Port, LED13_Pin);
+			x+=deplacement.dx;
+			y+=deplacement.dy;
+			if (x<0) x=0;
+			if (x>1227) x=1227;//1707-480
+			if (y<0) y=0;
+			if (y>394) y=394;//700-272
+			for (int i=0;i<=8;i++){
+				RemplirImage(x,y+34*i,image);
+				BSP_LCD_DrawBitmap(0, hauteur-34*i, (uint8_t*) image);
+			}
+			sprintf(text,"x=%4ld y=%4ld",x,y);
+			BSP_LCD_DisplayStringAtLine(0, (uint8_t*) text);
+		}
 		osDelay(100);
 	}
 	/* USER CODE END Display_fonction */
@@ -333,51 +317,99 @@ void Display_fonction(void const * argument)
 /* Private application code --------------------------------------------------*/
 /* USER CODE BEGIN Application */
 
- void RemplirImage(uint16_t x, uint16_t y, char* image){
-   int16_t largeur=480,hauteur=272,h=700,l=1707,offset=137;
-   uint16_t i,j,k,index=138;
-   FIL file;
-//   uint8_t* uwInternelBuffer;
- 	uwInternelBuffer = (uint8_t*) 0xC0260000;
-   unsigned int byteRead;
- 	TCHAR pathfile[]="mapENS.bmp";
-   char data[480];
+void RemplirImage(int32_t x, int32_t y, char* image){
+	int16_t largeur=480,hauteur=272,h=700,l=1707,offset=image[10];
+	uint32_t k,index=offset,j;
+	FIL file;
+	char text[50]={};
+	//   uint8_t* uwInternelBuffer;
+	//	uwInternelBuffer = (uint8_t*) 0xC0260000;
+//	unsigned int byteRead;
+//	TCHAR pathfile[]="mapENS.bmp";
+//	BYTE data[480];
+	FATFS fs0;
+	UINT br;
+	FRESULT val0,val1,val2,val3;
 
-   f_open(&file,pathfile,FA_READ); // on ne prend que le fichier
-   for(i=0;i<largeur;i++){//Chaque ligne
-     k=offset+2*(j*(l+1)+x);
-     f_lseek(&file,k);
-     f_read(&file, (TCHAR*) data, 480, &byteRead);
-     for(j=0;j<hauteur;j++){
-       image[index]=data[j];
-       index++;
-     }
-   }
- 	f_close(&file);
- }
+//	if (FATFS_LinkDriver(&SD_Driver, SDPath) == 0) {
+		/*##-2- Initialize the Directory Files pointers (heap) ###################*/
+		val0 = f_mount(&fs0, "", 0);
+		val1 = f_open(&file,"mapENS.bmp",FA_READ);
+		for(j=y;j<y+34;j++){//Chaque ligne  		y+hauteur
+//			if (j==34)
 
- void FabriquerEntete(char* image){
-   char entete[138];
-   FIL file;
-//   uint8_t* uwInternelBuffer;
-   uwInternelBuffer = (uint8_t*) 0xC0260000;
-   unsigned int byteRead;
-   TCHAR pathfile[]="mapENS.bmp";
-   f_open(&file,pathfile,FA_READ); // on ne prend que le fichier
-   f_read(&file, (TCHAR*) entete, 138, &byteRead);
-   for(int i=0;i<138;i++){
-     image[i]=entete[i];
-   }
-   image[18]=0xE0;
-   image[19]=0x01;
-   image[20]=0x00;
-   image[21]=0x00;
-   image[22]=0x10;
-   image[23]=0x01;
-   image[24]=0x00;
-   image[25]=0x00;
-   f_close(&file);
- }
+			k=offset+2*(j*(l+1)+x);
+//			sprintf(text,"j: %lu k %lu   %lu         ",j,k,f_size(&file));
+//			BSP_LCD_DisplayStringAtLine(6, (uint8_t*) text);
+
+			val2=f_lseek(&file,k);
+			val3=f_read(&file,(TCHAR *)image+index,2*largeur,&br);
+			// for(i=0;i<2*largeur;i++){
+			// 	image[index]=data[i];
+			index=index+2*largeur;
+			// }
+		}
+		//			f_read(&file,(TCHAR *)entete,sizeof entete,&br);
+		f_close(&file);
+//	} else {
+//		/* FatFs Initialization Error */
+//		Error_Handler();
+//	}
+
+	//	f_open(&file,pathfile,FA_READ); // on ne prend que le fichier
+	//	for(i=0;i<largeur;i++){//Chaque ligne
+	//		k=offset+2*(j*(l+1)+x);
+	//		f_lseek(&file,k);
+	//		f_read(&file, (TCHAR*) data, 480, &byteRead);
+	//		for(j=0;j<hauteur;j++){
+	//			image[index]=data[j];
+	//			index++;
+	//		}
+	//	}
+	//	f_close(&file);
+}
+
+void FabriquerEntete(char* image){
+	BYTE entete[138];
+	FIL file;
+	FATFS fs0;
+	UINT br;
+//	uint8_t* uwInternelBuffer;
+
+//	uwInternelBuffer = (uint8_t*) 0xC0260000;
+	//unsigned int byteRead;
+	//TCHAR pathfile[]="mapENS.bmp";
+
+
+	if (FATFS_LinkDriver(&SD_Driver, SDPath) == 0) {
+		/*##-2- Initialize the Directory Files pointers (heap) ###################*/
+		f_mount(&fs0, "", 0);
+		f_open(&file,"mapENS.bmp",FA_READ);
+		f_read(&file,(TCHAR *)entete,sizeof entete,&br);
+		f_close(&file);
+	} else {
+		/* FatFs Initialization Error */
+		Error_Handler();
+	}
+
+	//f_open(&file,pathfile,FA_READ); // on ne prend que le fichier
+	//f_read(&file, (TCHAR*) entete, 138, &byteRead);
+	for(int i=0;i<138;i++){
+		image[i]=entete[i];
+	}
+	image[18]=0xE0;
+	image[19]=0x01;
+	image[20]=0x00;
+	image[21]=0x00;
+
+	image[22]=0x22;
+	image[23]=0x00;
+//	image[22]=0x10;
+//	image[23]=0x01;
+	image[24]=0x00;
+	image[25]=0x00;
+	//f_close(&file);
+}
 
 /* USER CODE END Application */
 
